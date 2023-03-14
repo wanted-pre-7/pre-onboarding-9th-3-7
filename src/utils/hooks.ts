d5a87fb928bfe89ce2fb5a03e9bfc54af1a3bc59
ilgon0110 <moses011010@gmail.com>
2023-03-14 19:46:08 +0900

    feat: IChartData type 생성

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
 import { getChart } from "../apis/chart";
 
-export const getChartData = () => {
-  const [chartData, setChartData] = useState({ data: {}, isloading: true });
+export const getChartData: () => IChartData = () => {
+  const [chartData, setChartData] = useState({ data: {}, isLoading: true });
 
   useEffect(() => {
     getChart().then((res) => {
-      setChartData({ data: { ...res }, isloading: false });
+      setChartData({ data: { ...res }, isLoading: false });
     });
   }, []);
 
   return chartData;
 };
+
+interface IChartData {
+  data: {
+    [key: string]: {
+      id: string;
+      value_area: number;
+      value_bar: number;
+    };
+  };
+  isLoading: boolean;
+}
