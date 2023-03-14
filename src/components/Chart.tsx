5b2f2f5f3664d55f52c445b93d0bcee064bfc833
che-97 <rue015@gmail.com>
2023-03-15 01:10:20 +0900

    feat[#8, #9, #10, #11]: 필터링 기능 구현
    - 버튼 형태로, 버튼에서 선택한 id값과 동일한 id값을 가진 데이터 구역 하이라이트
    - 특정 데이터 구역을 클릭시 동일한 id값을 가진 데이터 구역 하이라이트

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -6,6 +6,7 @@ import type { IAreaData, IBarData, IChart } from "../types/chart";
 const Chart = ({ data }: IChart) => {
   const barColor = "#546E7A";
   const areaColor = "#E91E63";
+  const highlightColor = "#00b4f0";
   const chartHeight = 550;
 
   const chartRef = useRef(null);
@@ -26,6 +27,8 @@ const Chart = ({ data }: IChart) => {
   );
   const categoryData = Object.keys(data);
 
+  const ids = [...new Set(barData.map((d) => d.id))];
+
   const options: ApexOptions = {
     chart: {
       width: "100%",
@@ -34,6 +37,69 @@ const Chart = ({ data }: IChart) => {
       },
       selection: {
         enabled: false,
+      },
+      events: {
+        click: function (event, chartContext, config) {
+          const dataPointIndex = config.dataPointIndex;
+
+          if (config.seriesIndex > -1 && dataPointIndex > -1) {
+            const id = barData[dataPointIndex].id;
+            const prevZoom = chartContext.zoomPanSelection;
+            chartContext.updateOptions(
+              {
+                series: [
+                  {
+                    name: "Bar",
+                    type: "bar",
+                    data: barData.map((d, i) => ({
+                      ...d,
+                      fillColor: d.id === id ? highlightColor : "",
+                    })),
+                  },
+                  {
+                    name: "Area",
+                    type: "area",
+                    data: areaData,
+                  },
+                ],
+              },
+              false,
+            );
+
+            chartContext.zoomX(prevZoom.minX, prevZoom.maxX);
+          }
+        },
+        selection: function (chartContext, { xaxis, yaxis }) {
+          if (xaxis && xaxis.min && xaxis.max) {
+            const idList = [
+              ...new Set(
+                barData
+                  .slice(Math.floor(xaxis.min), Math.floor(xaxis.max))
+                  .map((d) => d.id),
+              ),
+            ];
+            chartContext.updateOptions(
+              {
+                series: [
+                  {
+                    name: "Bar",
+                    type: "bar",
+                    data: barData.map((d, i) => ({
+                      ...d,
+                      fillColor: idList.includes(d.id) ? highlightColor : "",
+                    })),
+                  },
+                  {
+                    name: "Area",
+                    type: "area",
+                    data: areaData,
+                  },
+                ],
+              },
+              false,
+            );
+          }
+        },
       },
     },
     stroke: {
@@ -99,8 +165,68 @@ const Chart = ({ data }: IChart) => {
     },
   ];
 
+  const chartFilterHandler = (id: string) => {
+    chartRef.current?.chart.updateOptions(
+      {
+        series: [
+          {
+            name: "Bar",
+            type: "bar",
+            data: barData.map((d) => ({
+              ...d,
+              fillColor: id == d.id ? highlightColor : "",
+            })),
+          },
+          {
+            name: "Area",
+            type: "area",
+            data: areaData,
+          },
+        ],
+      },
+      false,
+    );
+  };
+  const chartResetHandler = () => {
+    chartRef.current?.chart.updateOptions(
+      {
+        series: [
+          {
+            name: "Bar",
+            type: "bar",
+            data: barData.map((d) => ({
+              ...d,
+            })),
+          },
+          {
+            name: "Area",
+            type: "area",
+            data: areaData,
+          },
+        ],
+      },
+      false,
+    );
+  };
+
   return (
     <>
+      <div>
+        {ids.length > 0 &&
+          ids.map((id) => {
+            return (
+              <button
+                key={id}
+                onClick={() => {
+                  chartFilterHandler(id);
+                }}
+              >
+                {id}
+              </button>
+            );
+          })}
+        <button onClick={chartResetHandler}>reset</button>
+      </div>
       <div id="chart">
         <ApexCharts
           options={options}
