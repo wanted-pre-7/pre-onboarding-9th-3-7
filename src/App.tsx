5e9844efb5020992c98cc22fb54d9426e80cb38b
Haesol <osunpine@gmail.com>
2023-03-15 00:01:00 +0900

    chore: 패키지 정리 및  파일 이동
    - mock data 파일을 public으로 이동하였습니다.
    - 사용하지 않는 차트 라이브러리는 삭제하였습니다.
    - axios, react-router-dom, styled-components 설치하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Main from "./pages/Main";
+
 const App = () => {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 };
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Main />} />
+    </>,
+  ),
+);
 
 export default App;
