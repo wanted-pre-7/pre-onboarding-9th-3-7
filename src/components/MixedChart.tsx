0c966be0a7fda5f1a6cb7e615e31fde65c595106
ilgon0110 <moses011010@gmail.com>
2023-03-15 03:37:56 +0900

    feat[#8, #9, #10, #11]: ID(지역이름)별로 필터링 하는 기능 구현
    filter하는 handleFilter 함수 구현
    painPoint: apexChart는 상태가 비동기가 되는 순간 에러가 나버림.
    dataPointSelector나 onClick이벤트를 쓸때 useEffect로 비동기 상태처리를 꼭 해줘야함

---
+++ src/components/MixedChart.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { renderToString } from "react-dom/server";
+import CustomTooltip from "./Tooltip";
+
+const MixedChart = ({
+  idData,
+  barData,
+  areaData,
+  labels,
+  handleFilter,
+}: ChartPropsType) => {
+  const [change, setChange] = useState(true);
+  useEffect(() => {
+    if (barData && areaData) {
+      setChange(false);
+    }
+  }, [barData, areaData]);
+
+  if (change) {
+    return <div></div>;
+  }
+  return (
+    <Chart
+      series={[
+        {
+          name: "BAR",
+          type: "column",
+          data: barData,
+        },
+        {
+          name: "AREA",
+          type: "area",
+          data: areaData,
+        },
+      ]}
+      options={{
+        chart: {
+          events: {
+            dataPointSelection(e, chartContext, config) {
+              handleFilter(idData[config.dataPointIndex]);
+            },
+          },
+        },
+        stroke: {
+          width: [0, 2],
+          curve: "smooth",
+        },
+        plotOptions: {
+          bar: {
+            columnWidth: "50%",
+          },
+        },
+        labels: labels,
+        markers: {
+          size: 3,
+        },
+        yaxis: {
+          title: {
+            text: "values",
+          },
+          min: 0,
+        },
+        tooltip: {
+          enabled: true,
+          custom: ({ dataPointIndex }) => {
+            return renderToString(
+              <CustomTooltip
+                active={true}
+                data={{
+                  id: idData[dataPointIndex],
+                  area: areaData[dataPointIndex].y,
+                  bar: barData[dataPointIndex].y,
+                }}
+              />,
+            );
+          },
+        },
+      }}
+    ></Chart>
+  );
+};
+
+interface IData {
+  x: string;
+  y: number;
+  fillColor: string;
+}
+type ChartPropsType = {
+  idData: string[];
+  barData: IData[];
+  areaData: IData[];
+  labels: string[];
+  handleFilter: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | string,
+  ) => void;
+};
+
+export default MixedChart;
--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -1,89 +1,82 @@
-import Chart from "react-apexcharts";
-import { renderToString } from "react-dom/server";
-import CustomTooltip from "../components/Tooltip";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import MixedChart from "../components/MixedChart";
 import { getChartData } from "../utils/hooks";
 
 const ChartPage = () => {
   const { data, isLoading } = getChartData();
 
+  const labels = Object.keys(data).map((v) => v.split(" ")[1]);
+  const area = Object.values(data).map((v, i) => {
+    return { x: labels[i], y: v.value_area, fillColor: "#05f936" };
+  });
+  const bar = Object.values(data).map((v, i) => {
+    return { x: labels[i], y: v.value_bar, fillColor: "#9febfb" };
+  });
+  const idData = Object.values(data).map((v) => v.id);
+  const idList = [...new Set(idData)];
+  idList.push("선택해제");
+
+  const [barData, setBarData] = useState(bar);
+  const [areaData, setAreaData] = useState(area);
+
+  useEffect(() => {
+    setBarData(bar);
+    setAreaData(area);
+  }, [data]);
+
+  const handleFilter = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | string,
+  ) => {
+    const id = typeof e === "string" ? e : (e.target as HTMLButtonElement).id;
+    const selectedId = idData
+      .map((v, i) => (v === id ? i : null))
+      .filter((v) => v !== null);
+
+    setBarData(
+      barData.map((v, i) =>
+        selectedId.includes(i)
+          ? { ...v, fillColor: "#004f5f" }
+          : { ...v, fillColor: "#9febfb" },
+      ),
+    );
+    setAreaData(
+      areaData.map((v, i) =>
+        selectedId.includes(i)
+          ? { ...v, fillColor: "#022c0a" }
+          : { ...v, fillColor: "#05f936" },
+      ),
+    );
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  const labels = Object.keys(data).map((v) => v.split(" ")[1]);
-  const areaData = Object.values(data).map((v) => v.value_area);
-  const barData = Object.values(data).map((v) => v.value_bar);
-  const idData = Object.values(data).map((v) => v.id);
 
   return (
-    <Chart
-      series={[
-        {
-          name: "BAR",
-          type: "column",
-          data: barData,
-        },
-        {
-          name: "AREA",
-          type: "area",
-          data: areaData,
-        },
-      ]}
-      options={{
-        chart: {
-          events: {
-            xAxisLabelClick: () => {
-              console.log("x");
-            },
-          },
-        },
-        stroke: {
-          width: [0, 2],
-          curve: "smooth",
-        },
-        plotOptions: {
-          bar: {
-            columnWidth: "50%",
-          },
-        },
-        fill: {
-          opacity: [1, 0.2],
-          gradient: {
-            inverseColors: false,
-            shade: "light",
-            type: "vertical",
-            opacityFrom: 0.85,
-            opacityTo: 0.55,
-            stops: [0, 100, 100, 100],
-          },
-        },
-        labels: labels,
-        markers: {
-          size: 0,
-        },
-        yaxis: {
-          title: {
-            text: "values",
-          },
-          min: 0,
-        },
-        tooltip: {
-          enabled: true,
-          custom: ({ dataPointIndex }) => {
-            return renderToString(
-              <CustomTooltip
-                active={true}
-                data={{
-                  id: idData[dataPointIndex],
-                  area: areaData[dataPointIndex],
-                  bar: barData[dataPointIndex],
-                }}
-              />,
-            );
-          },
-        },
-      }}
-    ></Chart>
+    <Container>
+      <MixedChart
+        idData={idData}
+        barData={barData}
+        areaData={areaData}
+        labels={labels}
+        handleFilter={handleFilter}
+      />
+      {idList.map((id) => {
+        return (
+          <button key={id} onClick={(e) => handleFilter(e)} id={id}>
+            {id}
+          </button>
+        );
+      })}
+    </Container>
   );
 };
+
+const Container = styled.div`
+  width: 65%;
+  height: auto;
+  margin: 0 auto;
+`;
 
 export default ChartPage;
