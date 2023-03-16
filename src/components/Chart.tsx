3ed8fef7b12e57732fc70c2695dbd908f0055674
violetwhenisawu <anashape710@gmail.com>
2023-03-17 01:32:20 +0900

    refactor: hide 속성을 활용하여 차트 조건부 렌더링 버튼 추가

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -15,8 +15,6 @@ import useChartData from "../hooks/useChartData";
 import CustomizedDot from "./CustomizedDot";
 import CustomToolTip from "./CustomToolTip";
 
-type Category = "전체" | "area" | "bar";
-const CATEGORY: Category[] = ["전체", "area", "bar"];
 const Chart = ({
   district,
   handleClick,
@@ -25,28 +23,20 @@ const Chart = ({
   handleClick: (value: string) => void;
 }) => {
   const { data } = useChartData();
-  const [category, setCategory] = useState<Category>("전체");
 
+  const [hideBarChart, setHideBarChart] = useState(false);
   const [dot, setDot] = useState("");
-
-  const handleClickCategory = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setCategory(e.currentTarget.textContent as Category);
-  };
 
   return (
     <>
-      <div className="btn-wrapper">
-        {CATEGORY.map((item) => (
-          <button
-            className={`${item === category ? "btn-active" : "btn"}`}
-            key={item}
-            onClick={handleClickCategory}
-          >
-            {item}
-          </button>
-        ))}
-      </div>
-
+      <button
+        className="btn"
+        onClick={() => {
+          hideBarChart ? setHideBarChart(false) : setHideBarChart(true);
+        }}
+      >
+        {hideBarChart ? "전체 차트" : "Area 차트만 보기"}
+      </button>
       <ResponsiveContainer width="100%" height={600}>
         <ComposedChart
           data={data}
@@ -86,43 +76,42 @@ const Chart = ({
             }
           />
           <Legend height={50} />
-          {category === "전체" || category === "bar" ? (
-            <Bar
-              dataKey="value_bar"
-              barSize={20}
-              fill="#8884d8"
-              yAxisId="right"
-              onClick={(data) => handleClick(data.id)}
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={`${entry.id === district ? `#444094` : `#8884d8`}`}
-                />
-              ))}
-            </Bar>
-          ) : null}
-          {category === "전체" || category === "area" ? (
-            <Area
-              type="monotone"
-              dataKey="value_area"
-              fill="#82ca9d"
-              stroke="#82ca9d"
-              yAxisId="left"
-              onClick={() => {
-                handleClick(dot);
-              }}
-              dot={
-                <CustomizedDot
-                  cx={0}
-                  cy={0}
-                  stroke="#86d3a4"
-                  district={district}
-                  payload={{ id: "", time: "", value_area: 0, value_bar: 0 }}
-                />
-              }
-            />
-          ) : null}
+
+          <Bar
+            dataKey="value_bar"
+            barSize={20}
+            fill="#8884d8"
+            yAxisId="right"
+            onClick={(data) => handleClick(data.id)}
+            hide={hideBarChart}
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={`${entry.id === district ? `#444094` : `#8884d8`}`}
+              />
+            ))}
+          </Bar>
+
+          <Area
+            type="monotone"
+            dataKey="value_area"
+            fill="#82ca9d"
+            stroke="#82ca9d"
+            yAxisId="left"
+            onClick={() => {
+              handleClick(dot);
+            }}
+            dot={
+              <CustomizedDot
+                cx={0}
+                cy={0}
+                stroke="#86d3a4"
+                district={district}
+                payload={{ id: "", time: "", value_area: 0, value_bar: 0 }}
+              />
+            }
+          />
         </ComposedChart>
       </ResponsiveContainer>
     </>
