00266752bca4793cd0d88a8a6c4fd877ef73dd0b
che-97 <rue015@gmail.com>
2023-03-16 16:56:15 +0900

    feat: 범례 클릭시 chart toggle

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -15,117 +15,120 @@ import useChartData from "../hooks/useChartData";
 import CustomizedDot from "./CustomizedDot";
 import CustomToolTip from "./CustomToolTip";
 
-type Category = "전체" | "area" | "bar";
-const CATEGORY: Category[] = ["전체", "area", "bar"];
+const ChartCategory = {
+  BAR: "value_bar",
+  AREA: "value_area",
+} as const;
+type ChartCategoryAreaType = typeof ChartCategory.AREA;
+type ChartCategoryBarType = typeof ChartCategory.BAR;
+
+type Category = ChartCategoryAreaType | ChartCategoryBarType;
+
 const Chart = ({
   district,
   handleClick,
 }: {
-  district: string;
+  district: string | null;
   handleClick: (value: string) => void;
 }) => {
   const { data } = useChartData();
-  const [category, setCategory] = useState<Category>("전체");
-
+  const [category, setCategory] = useState<Category[]>([]);
   const [dot, setDot] = useState("");
 
-  const handleClickCategory = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setCategory(e.currentTarget.textContent as Category);
-  };
-
   return (
-    <>
-      <div className="btn-wrapper">
-        {CATEGORY.map((item) => (
-          <button
-            className={`${item === category ? "btn-active" : "btn"}`}
-            key={item}
-            onClick={handleClickCategory}
-          >
-            {item}
-          </button>
-        ))}
-      </div>
-
-      <ResponsiveContainer width="100%" height={600}>
-        <ComposedChart
-          data={data}
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 20,
-            left: 20,
+    <ResponsiveContainer width="100%" height={600}>
+      <ComposedChart
+        data={data}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+        barGap={10}
+      >
+        <CartesianGrid stroke="#f5f5f5" />
+        <XAxis dataKey="time" />
+        <YAxis
+          yAxisId={ChartCategory.AREA}
+          label={{
+            value: ChartCategory.AREA,
+            angle: -90,
+            position: "insideLeft",
+            offset: 1,
           }}
-          barGap={10}
-        >
-          <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="time" />
-          <YAxis
-            yAxisId="left"
-            label={{
-              value: "value_area",
-              angle: -90,
-              position: "insideLeft",
-              offset: 1,
-            }}
-          />
-          <YAxis
-            yAxisId="right"
-            orientation="right"
-            label={{
-              value: "value_bar",
-              angle: 90,
-              position: "insideRight",
+        />
+        <YAxis
+          yAxisId={ChartCategory.BAR}
+          orientation="right"
+          label={{
+            value: ChartCategory.BAR,
+            angle: 90,
+            position: "insideRight",
 
-              offset: -10,
-            }}
-          />
-          <Tooltip
-            content={
-              <CustomToolTip setDot={setDot} active={false} payload={[]} />
-            }
-          />
-          <Legend height={50} />
-          {category === "전체" || category === "bar" ? (
-            <Bar
-              dataKey="value_bar"
-              barSize={20}
-              fill="#8884d8"
-              yAxisId="right"
-              onClick={(data) => handleClick(data.id)}
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={`${entry.id === district ? `#444094` : `#8884d8`}`}
-                />
-              ))}
-            </Bar>
-          ) : null}
-          {category === "전체" || category === "area" ? (
-            <Area
-              type="monotone"
-              dataKey="value_area"
-              fill="#82ca9d"
-              stroke="#82ca9d"
-              yAxisId="left"
-              onClick={() => {
-                handleClick(dot);
+            offset: -10,
+          }}
+        />
+        <Tooltip
+          content={
+            <CustomToolTip setDot={setDot} active={false} payload={[]} />
+          }
+        />
+        <Legend
+          height={50}
+          onClick={(o) =>
+            setCategory(
+              category.includes(o.dataKey)
+                ? category.filter((key) => key !== o.dataKey)
+                : [...category, o.dataKey],
+            )
+          }
+        />
+
+        <Area
+          type="monotone"
+          dataKey={ChartCategory.AREA}
+          fill="#82ca9d"
+          stroke="#82ca9d"
+          yAxisId={ChartCategory.AREA}
+          onClick={() => {
+            handleClick(dot);
+          }}
+          dot={
+            <CustomizedDot
+              cx={0}
+              cy={0}
+              stroke="#86d3a4"
+              district={district}
+              payload={{
+                id: "",
+                time: "",
+                [ChartCategory.AREA]: 0,
+                [ChartCategory.BAR]: 0,
               }}
-              dot={
-                <CustomizedDot
-                  cx={0}
-                  cy={0}
-                  stroke="#86d3a4"
-                  district={district}
-                  payload={{ id: "", time: "", value_area: 0, value_bar: 0 }}
-                />
-              }
             />
-          ) : null}
-        </ComposedChart>
-      </ResponsiveContainer>
-    </>
+          }
+          hide={category.includes(ChartCategory.AREA)}
+        />
+
+        <Bar
+          dataKey={ChartCategory.BAR}
+          barSize={20}
+          fill="#8884d8"
+          yAxisId={ChartCategory.BAR}
+          onClick={(data) => handleClick(data.id)}
+          hide={category.includes(ChartCategory.BAR)}
+          opacity={0.6}
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={`${entry.id === district ? `#444094` : `#8884d8`}`}
+            />
+          ))}
+        </Bar>
+      </ComposedChart>
+    </ResponsiveContainer>
   );
 };
 export default Chart;
